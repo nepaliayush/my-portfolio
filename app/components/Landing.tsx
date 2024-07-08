b3317affea716711b24661233ddeace88b4d42d7
Ayush Nepali <anepali818@gmail.com>
2024-07-08 14:41:57 +0545

    Updated

--- app/components/Landing.tsx
+++ app/components/Landing.tsx
@@ -4,7 +4,9 @@ function Landing() {
   return (
     <div className="mx-4  md:mx-auto mt-5 flex flex-col items-center md:flex-row">
       <div className="w-96 md:w-1/2 flex justify-center md:justify-center mb-5 md:mb-0">
-        <Image src="ayushnepali1.png" alt="Photo" className="h-64 md:h-auto"/>
+        <Image src="ayushnepali1.png" alt="Photo" width={500}
+          height={500}
+        />
       </div>
       <div className="w-96 md:w-1/2 flex flex-col justify-center items-center text-center">
         <h1 className="text-4xl font-bold text-gray-900 mb-4 animate-fade-in-up">Ayush Nepali</h1>
