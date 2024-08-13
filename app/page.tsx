33567396818f5379ea93da7154cb39fa994b59c9
Ayush Nepali <anepali818@gmail.com>
2024-08-13 22:55:22 +0545

    fixeed scrolling i refresh

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect } from 'react';
 import Landing from "./components/Landing";
 import AboutMe from "./components/AboutMe";
 import Contact from "./components/Contact";
@@ -8,22 +9,33 @@ import Projects from "./components/Projects";
 import Footer from "./components/Footer";
 
 export default function Home() {
+  useEffect(() => {
+    if ('scrollRestoration' in history) {
+      history.scrollRestoration = 'manual';
+    }
+
+    return () => {
+      if ('scrollRestoration' in history) {
+        history.scrollRestoration = 'auto';
+      }
+    };
+  }, []);
+
   return (
     <main >
-      <section id="home"  className="h-screen">
+      <section id="home" className="h-screen">
         <Landing />
       </section>
 
-      <section id="about-me"  >
+      <section id="about-me">
         <AboutMe />
       </section>
-      <section id="experience" >
+      <section id="experience">
         <Experience />
       </section>
-      <section id="contact" >
+      <section id="contact">
         <Contact />
       </section>
-      
     </main>
   );
 }
